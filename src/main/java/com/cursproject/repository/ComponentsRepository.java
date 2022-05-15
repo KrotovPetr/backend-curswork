e2d3578a04904335738ed4a76f54f44b9aa419c2
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-05-15 13:16:53 +0300

    Add README.md

--- src/main/java/com/cursproject/repository/ComponentsRepository.java
+++ src/main/java/com/cursproject/repository/ComponentsRepository.java
@@ -2,9 +2,11 @@ package com.cursproject.repository;
 
 import com.cursproject.Entity.Components;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ComponentsRepository extends JpaRepository<Components, Long> {
     Optional<Components> findByName(String name);
 }
